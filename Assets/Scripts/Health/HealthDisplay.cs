33ecdbed56b850561efb3214092a269fd1d08e37
jonathanordrick <edrawijaya77@gmail.com>
2025-06-26 12:53:46 +0700

    Add EmojiOne sprite sheet and associated metadata for TextMesh Pro

---
+++ Assets/Scripts/Health/HealthDisplay.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthDisplay : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+
+    public Sprite emptyHeart;
+    public Sprite fullHeart;
+    public Sprite[] hearts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
